426c18c765b586f4ce23fd9e167465af295085b2
SLAC fphysics <zack@slac.stanford.edu>
2024-08-28 16:36:50 -0700

    mock p4p import in autodocs

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_mock_imports = ['epics', 'pytao']
+autodoc_mock_imports = ['epics', 'pytao', 'p4p']
 
 autodoc_member_order = 'groupwise'
 
