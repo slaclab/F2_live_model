a910139fcb9ad31ec7cc5d7e5e4c24504dbb5169
SLAC fphysics <zack@slac.stanford.edu>
2024-09-21 21:52:35 -0700

    Add LEM data to server, migrate to using config file, refactoring, streamlined interface

--- server.py
+++ server.py
@@ -5,14 +5,15 @@
 import os
 import sys
 import logging
-from logging.handlers import RotatingFileHandler
 import argparse
 import numpy as np
 import time
+import yaml
 from datetime import datetime
+from logging.handlers import RotatingFileHandler
 
 from epics import get_pv
-from p4p.nt import NTTable
+from p4p.nt import NTTable, NTScalar
 from p4p.server import Server as PVAServer
 from p4p.server.thread import SharedPV
 
@@ -23,15 +24,8 @@ PATH_SELF = os.path.dirname(os.path.abspath(__file__))
 DIR_SELF = os.path.join(*os.path.split(PATH_SELF)[:-1])
 sys.path.append(DIR_SELF)
 os.chdir(DIR_SELF)
+with open('config/facet2e.yaml') as f: CONFIG = yaml.safe_load(f)
 
-DIR_MODEL_DATA = '/u1/facet/physics/F2_live_model/'
-DIR_SERVER_LOGS = os.path.join(DIR_MODEL_DATA, 'logs')
-
-HEARTBEAT_CHANNEL = 'PHYS:SYS1:1:MODEL_SERVER'
-MODEL_NAME = 'FACET2E'
-TABLE_PV_STEM = f'BMAD:SYS0:1:{MODEL_NAME}'
-
-SERVER_UPDATE_INTERVAL = 1.0
 
 NTT_TWISS = NTTable([
     ("element", "s"), ("device_name", "s"),
@@ -50,11 +44,12 @@ NTT_RMAT = NTTable([
     ("r61", "d"), ("r62", "d"), ("r63", "d"), ("r64", "d"), ("r65", "d"), ("r66", "d"),
     ])
 
-NTT_LEM = NTTable([
+NTT_LEM_DATA = NTTable([
     ("element", "s"), ("device_name", "s"), ("s", "d"), ("z", "d"), ("length", "d"),
-    ("EREF","d"), ("EACT","d"), ("EERR","d"), 
-    ("BDES","d"), ("BLEM","d"), ("BLEM_save","d"), 
+    ("region", "s"), ("EREF","d"), ("EACT","d"), ("EERR","d"), ("BDES","d"), ("BLEM","d"),
     ])
+
+
 
 
 class f2LiveModelServer:
@@ -104,17 +99,37 @@ class f2LiveModelServer:
         self.PV_urmat_design = SharedPV(nt=NTT_RMAT, initial=design_urmat)
         self.PV_urmat_live =   SharedPV(nt=NTT_RMAT, initial=design_urmat)
 
+        self.PV_LEM_data =   SharedPV(nt=NTT_LEM_DATA, initial=None)
+        self.PV_LEM_fudges = [SharedPV(nt=NTScalar('d'), initial=1.0) for _ in range(4)]
+        self.PV_LEM_ampls =  [SharedPV(nt=NTScalar('d'), initial=1.0) for _ in range(4)]
+        self.PV_LEM_chirps = [SharedPV(nt=NTScalar('d'), initial=1.0) for _ in range(4)]
+
         # mapping of PV names to SharedPV objects for use by the PVAServer
+        pv_root = f"{CONFIG['server']['PV']['provider_stem']}:{CONFIG['name']}"
         self.provider = {
-            f'{TABLE_PV_STEM}:DESIGN:TWISS': self.PV_twiss_design,
-            f'{TABLE_PV_STEM}:LIVE:TWISS':   self.PV_twiss_live,
-            f'{TABLE_PV_STEM}:DESIGN:RMAT':  self.PV_rmat_design,
-            f'{TABLE_PV_STEM}:LIVE:RMAT':    self.PV_rmat_live,
-            f'{TABLE_PV_STEM}:DESIGN:URMAT': self.PV_urmat_design,
-            f'{TABLE_PV_STEM}:LIVE:URMAT':   self.PV_urmat_live,
+            f'{pv_root}:DESIGN:TWISS': self.PV_twiss_design,
+            f'{pv_root}:LIVE:TWISS':   self.PV_twiss_live,
+            f'{pv_root}:DESIGN:RMAT':  self.PV_rmat_design,
+            f'{pv_root}:LIVE:RMAT':    self.PV_rmat_live,
+            f'{pv_root}:DESIGN:URMAT': self.PV_urmat_design,
+            f'{pv_root}:LIVE:URMAT':   self.PV_urmat_live,
+
+            f'{pv_root}:LEM:DATA':     self.PV_LEM_data,
+            f'{pv_root}:LEM:L0_FUDGE': self.PV_LEM_fudges[0],
+            f'{pv_root}:LEM:L1_FUDGE': self.PV_LEM_fudges[1],
+            f'{pv_root}:LEM:L2_FUDGE': self.PV_LEM_fudges[2],
+            f'{pv_root}:LEM:L3_FUDGE': self.PV_LEM_fudges[3],
+            f'{pv_root}:LEM:L0_AMPL':  self.PV_LEM_ampls[0]
+            f'{pv_root}:LEM:L1_AMPL':  self.PV_LEM_ampls[1]
+            f'{pv_root}:LEM:L2_AMPL':  self.PV_LEM_ampls[2]
+            f'{pv_root}:LEM:L3_AMPL':  self.PV_LEM_ampls[3]
+            f'{pv_root}:LEM:L0_CHIRP': self.PV_LEM_chirps[0],
+            f'{pv_root}:LEM:L1_CHIRP': self.PV_LEM_chirps[1],
+            f'{pv_root}:LEM:L2_CHIRP': self.PV_LEM_chirps[2],
+            f'{pv_root}:LEM:L3_CHIRP': self.PV_LEM_chirps[3],
             }
 
-        self.PV_heartbeat = get_pv(HEARTBEAT_CHANNEL)
+        self.PV_heartbeat = get_pv(CONFIG['server']['PV']['heartbeat'])
 
     def run(self):
         """
@@ -129,12 +144,16 @@ class f2LiveModelServer:
                 hb = 0
                 while True:
                     hb = np.mod(hb + 1, 100)
-                    time.sleep(SERVER_UPDATE_INTERVAL)
+                    time.sleep(CONFIG['server']['poll_interval'])
                     self.PV_heartbeat.put(hb, 100)
                     if self.design_only: continue
                     self.PV_twiss_live.post(self._get_twiss_table(which='model'))
                     self.PV_rmat_live.post(self._get_rmat_table(which='model', combined=True))
                     self.PV_urmat_live.post(self._get_rmat_table(which='model', combined=False))
+                    for i, region in enumerate(self.model.LEM):
+                        self.PV_LEM_ampls[i].post(region.amplitude)
+                        self.PV_LEM_chirps[i].post(region.chirp)
+                        self.PV_LEM_fudges[i].post(region.fudge)
             except KeyboardInterrupt:
                 pass
             finally:
@@ -183,43 +202,49 @@ class f2LiveModelServer:
         return NTT_RMAT.wrap(rows)
 
     def _get_LEM_table(self):
-        return
+        rows = []
+        for region in self.model.LEM:
+            for i, ele in enumerate(region.elements):
+                i_global = self.model.ix[ele]
+                rows.append({
+                    **static_params[i_global]
+                    "region": region.name,
+                    "EREF" : region.EREF[i],
+                    "EACT" : region.EACT[i],
+                    "EERR" : region.EERR[i],
+                    "BDES" : region.BDES[i],
+                    "BLEM" : region.BLEM[i],
+                    })
+        return NTT_LEM_DATA.wrap(rows)
 
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Live model service")
-    parser.add_argument(
-        '--log_level', help='Configure level of log display',
-        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-        default='INFO'
-        )
-    parser.add_argument(
-        '--log_dir', help='Directory where logs get saved.',
-        default=DIR_SERVER_LOGS
-        )
     parser.add_argument(
         '--design_only', help='Serve design model data only. "LIVE" PVs will be static.',
         action='store_true'
         )
     args = parser.parse_args()
 
-    timestamp = datetime.today().strftime('%y%m%d%H%M%S')
-
+    timestamp = datetime.today().strftime(CONFIG['ts_fmt'])
     stream_handler = logging.StreamHandler()
     logfile_handler = RotatingFileHandler(
-        os.path.join(args.log_dir, f'live_model_{timestamp}.log'),
-        maxBytes=10*1024*1024, backupCount=10,
+        os.path.join(CONFIG['dirs']['server_logs'], f"{CONFIG['name']}_model_server_{timestamp}.log"),
+        maxBytes=CONFIG['server']['logs']['max_MB']*1024*1024,
+        backupCount=CONFIG['server']['logs']['N_backup'],
         )
     logging.basicConfig(
         handlers=[stream_handler, logfile_handler],
         level=args.log_level,
-        format="%(asctime)s.%(msecs)03d [F2ModelServer] %(levelname)s: %(message)s ",
-        datefmt="%Y-%m-%d %H:%M:%S",
+        format=CONFIG['server']['logs']['fmt'],
+        datefmt=CONFIG['dt_fmt'],
         force=True
         )
 
     service = f2LiveModelServer(
-        design_only=args.design_only, log_level=args.log_level, log_handler=logfile_handler
+        design_only=args.design_only,
+        log_level=CONFIG['server']['logs']['level'],
+        log_handler=logfile_handler
         )
 
     service.run()
