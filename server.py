1083ac27475ee287dddb869fdd3a09928ba8001e
SLAC fphysics <zack@slac.stanford.edu>
2024-09-23 13:35:43 -0700

    split RMAT publication into a separate service

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ import time
 import yaml
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
+from threading import Event
 
 from epics import get_pv
 from p4p.nt import NTTable, NTScalar
@@ -25,7 +26,7 @@ DIR_SELF = os.path.join(*os.path.split(PATH_SELF)[:-1])
 DIR_CONFIG = os.path.join(os.path.join(DIR_SELF, 'F2_live_model', 'config'))
 sys.path.append(DIR_SELF)
 os.chdir(DIR_SELF)
-with open(os.path.join(DIR_CONFIG, 'facet2e.yaml')) as f:
+with open(os.path.join(DIR_CONFIG, 'facet2e.yaml'), 'r') as f:
     CONFIG = yaml.safe_load(f)
 
 
@@ -48,10 +49,8 @@ NTT_RMAT = NTTable([
 
 NTT_LEM_DATA = NTTable([
     ("element", "s"), ("device_name", "s"), ("s", "d"), ("z", "d"), ("length", "d"),
-    ("region", "s"), ("EREF","d"), ("EACT","d"), ("EERR","d"), ("BDES","d"), ("BLEM","d"),
+    ("region", "s"), ("EREF","d"), ("EACT","d"), ("EERR","d"), ("BLEM","d"),
     ])
-
-
 
 
 class f2LiveModelServer:
@@ -63,22 +62,19 @@ class f2LiveModelServer:
         source is ``LIVE`` or ``DESIGN`` and data is ``TWISS``, ``RMAT`` or ``URMAT``.
 
     :note: Running ``server.py`` as a script will start the service.
-
     :note: When active, this service will update a heartbeat PV: ``PHYS:SYS1:1:MODEL_SERVER``
     """
-
     def __init__(self, design_only=False, log_level='INFO', log_handler=None):
-
         self.design_only = design_only
+        self.log_level = log_level
+        self.log_handler = log_handler
+        self.pv_root = f"{CONFIG['server']['PV']['provider_stem']}:{CONFIG['name']}"
+        self.PV_heartbeat = get_pv(CONFIG['server']['PV']['heartbeat'])
+        self._interrupt = Event()
 
-        logging.info("Initializing BmadLiveModel ...")
-        self.model = BmadLiveModel(
-            design_only=self.design_only,
-            log_level=log_level,
-            log_handler=log_handler
-            )
-
+    def _load_static_device_data(self):
         # store a list of static device info - this gets reused a lot
+        # must be called after model initialization
         self._static_device_data = []
         for i, ele_name in enumerate(self.model.elements):
             self._static_device_data.append({
@@ -89,78 +85,108 @@ class f2LiveModelServer:
                 'length': self.model.L[i],
                 })
 
+    def _run_main_server(self):
+        # run the primary server with the real-time model that publishes LEM/Twiss data
+        self.model = BmadLiveModel(
+            design_only=self.design_only,
+            log_level=self.log_level,
+            log_handler=self.log_handler,
+            )
+        self._load_static_device_data()
+        self.model.start()
+
         # initialize all PVs with their design values
         design_twiss = self._get_twiss_table(which='design')
-        design_rmat = self._get_rmat_table(which='design', combined=True)
-        design_urmat = self._get_rmat_table(which='design', combined=False)
-
         self.PV_twiss_design = SharedPV(nt=NTT_TWISS, initial=design_twiss)
         self.PV_twiss_live =   SharedPV(nt=NTT_TWISS, initial=design_twiss)
+        self.PV_LEM_data =     SharedPV(nt=NTT_LEM_DATA, initial=self._get_LEM_table())
+        self.PV_LEM_fudges =   [SharedPV(nt=NTScalar('d'), initial=1.0) for _ in range(4)]
+        self.PV_LEM_ampls =    [SharedPV(nt=NTScalar('d'), initial=1.0) for _ in range(4)]
+        self.PV_LEM_chirps =   [SharedPV(nt=NTScalar('d'), initial=1.0) for _ in range(4)]
+        self.provider = {
+            f'{self.pv_root}:DESIGN:TWISS': self.PV_twiss_design,
+            f'{self.pv_root}:LIVE:TWISS':   self.PV_twiss_live,
+            f'{self.pv_root}:LEM:DATA':     self.PV_LEM_data,
+            f'{self.pv_root}:LEM:L0_FUDGE': self.PV_LEM_fudges[0],
+            f'{self.pv_root}:LEM:L1_FUDGE': self.PV_LEM_fudges[1],
+            f'{self.pv_root}:LEM:L2_FUDGE': self.PV_LEM_fudges[2],
+            f'{self.pv_root}:LEM:L3_FUDGE': self.PV_LEM_fudges[3],
+            f'{self.pv_root}:LEM:L0_AMPL':  self.PV_LEM_ampls[0],
+            f'{self.pv_root}:LEM:L1_AMPL':  self.PV_LEM_ampls[1],
+            f'{self.pv_root}:LEM:L2_AMPL':  self.PV_LEM_ampls[2],
+            f'{self.pv_root}:LEM:L3_AMPL':  self.PV_LEM_ampls[3],
+            f'{self.pv_root}:LEM:L0_CHIRP': self.PV_LEM_chirps[0],
+            f'{self.pv_root}:LEM:L1_CHIRP': self.PV_LEM_chirps[1],
+            f'{self.pv_root}:LEM:L2_CHIRP': self.PV_LEM_chirps[2],
+            f'{self.pv_root}:LEM:L3_CHIRP': self.PV_LEM_chirps[3],
+            }
+        with PVAServer(providers=[self.provider]):
+            hb = 0
+            while not self._interrupt.wait(CONFIG['server']['poll_interval']):
+                hb = np.mod(hb + 1, 100)
+                self.PV_heartbeat.put(hb, 100)
+                if self.design_only: continue
+                self.PV_twiss_live.post(self._get_twiss_table(which='model'))
+                self.PV_LEM_data.post(self._get_LEM_table())
+                for i, region in enumerate(self.model.LEM):
+                    self.PV_LEM_ampls[i].post(region.amplitude)
+                    self.PV_LEM_chirps[i].post(region.chirp)
+                    self.PV_LEM_fudges[i].post(region.fudge)
+            else:
+                self.model.stop()
+
+    def _run_RMAT_server(self):
+        # run the secondary server that published RMATs for the whole machine
+        # setup a second instanced model just for making Rmats
+        self.model = BmadLiveModel(
+            instanced=True,
+            log_level=self.log_level,
+            log_handler=self.log_handler,
+            )
+        self._load_static_device_data()
+        design_rmat = self._get_rmat_table(which='design', combined=True)
+        design_urmat = self._get_rmat_table(which='design', combined=False)
         self.PV_rmat_design =  SharedPV(nt=NTT_RMAT, initial=design_rmat)
         self.PV_rmat_live =    SharedPV(nt=NTT_RMAT, initial=design_rmat)
         self.PV_urmat_design = SharedPV(nt=NTT_RMAT, initial=design_urmat)
         self.PV_urmat_live =   SharedPV(nt=NTT_RMAT, initial=design_urmat)
-
-        self.PV_LEM_data =   SharedPV(nt=NTT_LEM_DATA, initial=None)
-        self.PV_LEM_fudges = [SharedPV(nt=NTScalar('d'), initial=1.0) for _ in range(4)]
-        self.PV_LEM_ampls =  [SharedPV(nt=NTScalar('d'), initial=1.0) for _ in range(4)]
-        self.PV_LEM_chirps = [SharedPV(nt=NTScalar('d'), initial=1.0) for _ in range(4)]
-
-        # mapping of PV names to SharedPV objects for use by the PVAServer
-        pv_root = f"{CONFIG['server']['PV']['provider_stem']}:{CONFIG['name']}"
         self.provider = {
-            f'{pv_root}:DESIGN:TWISS': self.PV_twiss_design,
-            f'{pv_root}:LIVE:TWISS':   self.PV_twiss_live,
-            f'{pv_root}:DESIGN:RMAT':  self.PV_rmat_design,
-            f'{pv_root}:LIVE:RMAT':    self.PV_rmat_live,
-            f'{pv_root}:DESIGN:URMAT': self.PV_urmat_design,
-            f'{pv_root}:LIVE:URMAT':   self.PV_urmat_live,
-
-            f'{pv_root}:LEM:DATA':     self.PV_LEM_data,
-            f'{pv_root}:LEM:L0_FUDGE': self.PV_LEM_fudges[0],
-            f'{pv_root}:LEM:L1_FUDGE': self.PV_LEM_fudges[1],
-            f'{pv_root}:LEM:L2_FUDGE': self.PV_LEM_fudges[2],
-            f'{pv_root}:LEM:L3_FUDGE': self.PV_LEM_fudges[3],
-            f'{pv_root}:LEM:L0_AMPL':  self.PV_LEM_ampls[0],
-            f'{pv_root}:LEM:L1_AMPL':  self.PV_LEM_ampls[1],
-            f'{pv_root}:LEM:L2_AMPL':  self.PV_LEM_ampls[2],
-            f'{pv_root}:LEM:L3_AMPL':  self.PV_LEM_ampls[3],
-            f'{pv_root}:LEM:L0_CHIRP': self.PV_LEM_chirps[0],
-            f'{pv_root}:LEM:L1_CHIRP': self.PV_LEM_chirps[1],
-            f'{pv_root}:LEM:L2_CHIRP': self.PV_LEM_chirps[2],
-            f'{pv_root}:LEM:L3_CHIRP': self.PV_LEM_chirps[3],
+            f'{self.pv_root}:DESIGN:RMAT':  self.PV_rmat_design,
+            f'{self.pv_root}:LIVE:RMAT':    self.PV_rmat_live,
+            f'{self.pv_root}:DESIGN:URMAT': self.PV_urmat_design,
+            f'{self.pv_root}:LIVE:URMAT':   self.PV_urmat_live,
             }
+        with PVAServer(providers=[self.provider]):
+            hb = 0
+            while not self._interrupt.wait(CONFIG['server']['poll_interval']):
+                hb = np.mod(hb + 1, 100)
+                self.PV_heartbeat.put(hb, 100)
+                if self.design_only: continue
+                self.model.refresh_all()
+                RMATs = self._get_rmat_table(which='model', combined=True)
+                URMATs = self._get_rmat_table(which='model', combined=False)
+                self.PV_rmat_live.post(RMATs)
+                self.PV_urmat_live.post(URMATs)
+            else:
+                raise self.model.stop()
 
-        self.PV_heartbeat = get_pv(CONFIG['server']['PV']['heartbeat'])
-
-    def run(self):
+    def run(self, rmats=False):
         """
         connect the BmadLiveModel to the accelerator and begins updating PVs with live data
 
+        :param primary: (optional) set to False to run the RMAT server instead
         :note: this function will execute forever until either an exception occurs or a 
             ``KeyboardInterrupt`` is provided to signal a stop
         """
         logging.info('Starting FACET Bmad live model service ...')
-        with self.model, PVAServer(providers=[self.provider]):
-            try:
-                hb = 0
-                while True:
-                    hb = np.mod(hb + 1, 100)
-                    time.sleep(CONFIG['server']['poll_interval'])
-                    self.PV_heartbeat.put(hb, 100)
-                    if self.design_only: continue
-                    self.PV_twiss_live.post(self._get_twiss_table(which='model'))
-                    self.PV_rmat_live.post(self._get_rmat_table(which='model', combined=True))
-                    self.PV_urmat_live.post(self._get_rmat_table(which='model', combined=False))
-                    self.PV_LEM_data.post(self._get_LEM_table())
-                    for i, region in enumerate(self.model.LEM):
-                        self.PV_LEM_ampls[i].post(region.amplitude)
-                        self.PV_LEM_chirps[i].post(region.chirp)
-                        self.PV_LEM_fudges[i].post(region.fudge)
-            except KeyboardInterrupt:
-                pass
-            finally:
-                logging.info("Stopping service.")
+        logging.info(f' --> Running the {"primary" if not rmats else "RMAT"} server ...')
+        runner = self._run_main_server if not rmats else self._run_RMAT_server
+        try:
+            runner()
+        except KeyboardInterrupt:
+            self._interrupt.set()
+        finally:
+            logging.info("Stopping service.")
 
     def _get_twiss_table(self, which='model'):
         # returns a table of twiss parameters at each element
@@ -189,17 +215,16 @@ class f2LiveModelServer:
     def _get_rmat_table(self, which='model', combined=False):
         # makes a table of rmats for all elements -- single-element maps by default,
         # if the 'combined' flag is set, will calculate the maps from the first element,
+        # NOTE: this method queries model to avoid bottlenecking model
+        
+        RMATs = self.model.get_all_rmats(combined=combined)
         rows = []
         for ix_ele, static_params in enumerate(self._static_device_data):
-            elem = (1, ix_ele) if combined else ix_ele
-            R, _ = self.model.get_rmat(elem, which=which)
-
             # pack the 6x6 matrix into a dict with keys like 'r11', 'r12', ...
             rmat_dict = {}
             for i in range(6):
                 for j in range(6):
-                    rmat_dict[f'r{i+1}{j+1}'] = R[i][j]
-
+                    rmat_dict[f'r{i+1}{j+1}'] = RMATs[ix_ele][i][j]
             rows.append({**static_params, **rmat_dict})
 
         return NTT_RMAT.wrap(rows)
@@ -209,13 +234,13 @@ class f2LiveModelServer:
         for region in self.model.LEM:
             for i, ele in enumerate(region.elements):
                 i_global = self.model.ix[ele]
+                static_params = self._static_device_data[i_global]
                 rows.append({
-                    **static_params[i_global],
+                    **static_params,
                     "region": region.name,
                     "EREF" : region.EREF[i],
                     "EACT" : region.EACT[i],
                     "EERR" : region.EERR[i],
-                    "BDES" : region.BDES[i],
                     "BLEM" : region.BLEM[i],
                     })
         return NTT_LEM_DATA.wrap(rows)
@@ -227,12 +252,18 @@ if __name__ == "__main__":
         '--design_only', help='Serve design model data only. "LIVE" PVs will be static.',
         action='store_true'
         )
+    parser.add_argument(
+        '--rmats', help='Run the alternate server process that updates live R matrices',
+        action='store_true'
+        )
     args = parser.parse_args()
 
+    srv_type = "primary" if args.rmats else "RMAT"
     timestamp = datetime.today().strftime(CONFIG['ts_fmt'])
+
     stream_handler = logging.StreamHandler()
     logfile_handler = RotatingFileHandler(
-        os.path.join(CONFIG['dirs']['server_logs'], f"{CONFIG['name']}_model_server_{timestamp}.log"),
+        os.path.join(CONFIG['dirs']['server_logs'], f"{CONFIG['name']}_{srv_type}_{timestamp}.log"),
         maxBytes=CONFIG['server']['logs']['max_MB']*1024*1024,
         backupCount=CONFIG['server']['logs']['N_backup'],
         )
@@ -247,8 +278,7 @@ if __name__ == "__main__":
     service = f2LiveModelServer(
         design_only=args.design_only,
         log_level=CONFIG['server']['logs']['level'],
-        log_handler=logfile_handler
+        log_handler=logfile_handler,
         )
-
-    service.run()
+    service.run(rmats=args.rmats)
 
